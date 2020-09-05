ce2e41f323291c0dbe5a9819e1d422b5394b41b9
Uyirendiren Chemen <udchemen@gmail.com>
2020-09-05 14:56:57 -0400

    mysql connector setup

--- main.py
+++ main.py
@@ -2,33 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from patientAPI import patient_api
 import os
+import sqlalchemy
 import models
 
 app = Flask(__name__)
 
-db_user = os.environ.get('CLOUD_SQL_USERNAME')
-db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:HikPl7fLHeu6lbvk@35.227.99.155/patient-db'
-db = SQLAlchemy(app)
+# db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_user = 'root'
+# db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_password = 'HikPl7fLHeu6lbvk'
+# db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_name = 'patient_db'
+# db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+db_connection_name = 'flask-gcloud:us-east1:patient-db'
+db_socket_dir = os.environ.get("/cloudsql")
 
 
-class Patient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(25), nullable=False)
-    last_name = db.Column(db.String(25), nullable=False)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ThisIsSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+mysqlconnector://' + db_user + ':' + db_password + '@35.227.99.155/patient_db?auth_plugin=mysql_native_password'
 
-
-db.create_all()
-db.session.commit()
-
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SECRET_KEY'] = 'ThisIsSecretKey'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:HikPl7fLHeu6lbvk@35.227.99.155/patient_db?auth_plugin=mysql_native_password'
-# app.config[
-#     'SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://' + db_user + ':' + db_password + '@' + db_connection_name + '/' + db_name + '?auth_plugin=mysql_native_password'
-# print(db_user + db_password + db_name + db_connection_name)
 # models.init_app(app)
 # models.create_tables(app)
 app.register_blueprint(patient_api)
--- models.py
+++ models.py
@@ -1,26 +1,26 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import create_engine
-# from sqlalchemy_utils import database_exists, create_database
-#
-# db = SQLAlchemy()
-#
-#
-# def init_app(app):
-#     db.app = app
-#     db.init_app(app)
-#     return db
-#
-#
-# def create_tables(app):
-#     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-#     if not database_exists(engine.url):
-#         create_database(engine.url)
-#         db.metadata.create_all(engine)
-#         db.session.commit()
-#     return engine
-#
-#
-# class Patient(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String(25), nullable=False)
-#     last_name = db.Column(db.String(25), nullable=False)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+db = SQLAlchemy()
+
+
+def init_app(app):
+    db.app = app
+    db.init_app(app)
+    return db
+
+
+def create_tables(app):
+    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    if not database_exists(engine.url):
+        create_database(engine.url)
+        db.metadata.create_all(engine)
+        db.session.commit()
+    return engine
+
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
